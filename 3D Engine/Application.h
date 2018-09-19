288dd825ac7ef4c925d5066816cb8fde5d58a3dc
GuillemArman <guillemarman8@gmail.com>
2018-09-19 12:30:10 +0200

    Erased P2 substituted STL

--- 3D Engine/Application.h
+++ 3D Engine/Application.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "p2List.h"
+
 #include "Globals.h"
 #include "Timer.h"
 #include "Module.h"
@@ -22,12 +22,12 @@ public:
 	ModuleCamera3D* camera;
 	ModulePhysics3D* physics;
 	ModuleImGui* imgui;
-
+	
 private:
 
 	Timer	ms_timer;
 	float	dt;
-	p2List<Module*> list_modules;
+	std::list<Module*> list_modules;
 
 public:
 
