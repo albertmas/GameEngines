f2562a5aa5b74419e9efc486c2fbb8e9409d4305
GuillemArman <guillemarman8@gmail.com>
2018-09-27 11:03:44 +0200

    Erased glew (too many problems)

--- 3D Engine/ModuleRenderer3D.cpp
+++ 3D Engine/ModuleRenderer3D.cpp
@@ -5,11 +5,12 @@
 #include "ModuleAudio.h"
 #include "ModuleCamera3D.h"
 #include "ModulePhysics3D.h"
-
+#include "SDL\include\SDL_opengl.h"
+#include "SDL\include\SDL.h"
 
 #pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
-#pragma comment (lib, "Glew/libx86/glew32.lib") /* link Microsoft OpenGL lib   */
+
 
 	
 ModuleRenderer3D::ModuleRenderer3D(bool start_enabled) : Module(start_enabled)
