653222195a6e410d4f60f14df7e9864f64f2145f
GuillemArman <guillemarman8@gmail.com>
2018-10-16 14:28:30 +0200

    Component.h WiP

---
+++ IrrealEngine/Component.h
@@ -0,0 +1,32 @@
+//#ifndef _COMPONENT_H_
+//#define _COMPONENT_H_
+//
+//#include "Globals.h"
+//
+//
+//class Component
+//{
+//public:
+//
+//	Component();
+//	~Component();
+//	
+//	virtual void Enable();
+//	virtual void Update();
+//	virtual void Disable();
+//
+//
+//
+//public:
+//
+//	//Game Object
+//	bool active;
+//	
+//
+//
+//
+//
+//
+//}
+//
+//#endif
