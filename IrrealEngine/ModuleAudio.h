021aa23663f606a97543297a397134771d9d0ce3
GuillemArman <guillemarman8@gmail.com>
2018-12-19 01:56:39 +0100

    Added module Audio

---
+++ IrrealEngine/ModuleAudio.h
@@ -0,0 +1,23 @@
+
+
+/*#include "SDL_mixer\include\SDL_mixer.h"
+#define DEFAULT_MUSIC_FADE_TIME 2.0f
+class ModuleAudio : public Module
+{
+public:
+ModuleAudio(bool start_enabled = true);
+~ModuleAudio();
+bool Init(JSON_File*conf);
+bool CleanUp();
+// Play a music file
+bool PlayMusic(const char* path, float fade_time = DEFAULT_MUSIC_FADE_TIME);
+// Load a WAV in memory
+unsigned int LoadFx(const char* path);
+// Play a previously loaded WAV
+bool PlayFx(unsigned int fx, int repeat = 0);
+void ImGuiDraw();
+private:
+Mix_Music*			music;
+list<Mix_Chunk*>	fx;
+};*/
+
